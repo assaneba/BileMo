c5865e0e010b720816a305a024935eff6b4e3a49
Assane Thione Ba <assanetb@gmail.com>
2020-05-26 00:48:19 +0000

    basic User registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class SecurityController
@@ -26,6 +31,25 @@ class SecurityController extends AbstractController
     public function login()
     {
         $user = $this->getUser();
+
+        return $user;
+    }
+
+    /**
+     * @Rest\Post(
+     *     path="/register",
+     *     name="register"
+     * )
+     * @Rest\View(statusCode= 201)
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     */
+    public function register(User $user, UserPasswordEncoderInterface $encoder, EntityManagerInterface $manager)
+    {
+        $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+        $user->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($user);
+        $manager->flush();
 
         return $user;
     }
